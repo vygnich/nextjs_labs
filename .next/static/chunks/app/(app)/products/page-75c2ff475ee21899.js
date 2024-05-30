3638009dea30fbc2f210ede2f2970208e30a8b8d
“Alex” <64835827+vygnich@users.noreply.github.com>
2024-05-31 00:42:40 +0300

    changed prisma version

--- .next/static/chunks/app/(app)/products/page-75c2ff475ee21899.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[126],{806:function(n,e,u){Promise.resolve().then(u.bind(u,8082))}},function(n){n.O(0,[122,231,82,971,23,744],function(){return n(n.s=806)}),_N_E=n.O()}]);
